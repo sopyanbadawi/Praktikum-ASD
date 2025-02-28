3f93101cab346b5a69352ebca96e6ae75935e6c9
sopyanbadawi <sopyanbadawi.com>
2025-02-28 14:52:49 +0700

    Percobaan 2 digabung pertanyaan

--- Jobsheet 3/MahsiswaDemo04.java
+++ Jobsheet 3/MahsiswaDemo04.java
@@ -1,6 +1,40 @@
+import java.util.Scanner;
 public class MahsiswaDemo04 {
     public static void main(String[] args) {
         Mahasiswa04[] arrayOfMahasiswa = new Mahasiswa04[3];
+        Scanner sc = new Scanner(System.in);
+        String dummy;
+        
+        for (int i = 0; i < 3; i++) {
+            arrayOfMahasiswa[i] = new Mahasiswa04();
+            
+            System.out.println("Masukkan Data Mahasiswa ke-" + (i+1));
+            System.out.print("NIM: ");
+            arrayOfMahasiswa[i].nim = sc.nextLine(); 
+            System.out.print("Nama: ");
+            arrayOfMahasiswa[i].nama = sc.nextLine();
+            System.out.print("Kelas: ");
+            arrayOfMahasiswa[i].kelas = sc.nextLine(); 
+            System.out.print("IPK: ");
+            dummy = sc.nextLine();
+            arrayOfMahasiswa[i].ipk = Float.parseFloat(dummy);
+            System.out.println("---------------------------------");
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Data Mahasiswa ke-" + (i+1));
+            arrayOfMahasiswa[i].cetakInfo();
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Data Mahasiswa ke-" + (i+1));
+            System.out.println("NIM: " + arrayOfMahasiswa[i].nim);
+            System.out.println("Nama: " + arrayOfMahasiswa[i].nama);
+            System.out.println("Kelas: " + arrayOfMahasiswa[i].kelas);
+            System.out.println("IPK: " + arrayOfMahasiswa[i].ipk);
+            System.out.println("---------------------------------");
+        }
+
         arrayOfMahasiswa[0] = new Mahasiswa04();
         arrayOfMahasiswa[0].nim = "244107060033";
         arrayOfMahasiswa[0].nama = "AGNES TITANIA KINANTI";
