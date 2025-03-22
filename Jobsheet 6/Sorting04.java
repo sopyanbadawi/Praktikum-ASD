d6283d046ebebd634e6f13372c54fde231c6af3f
sopyanbadawi <sopyanbadawi.com>
2025-03-23 01:06:36 +0700

    Sorting - Selection Sort

---
+++ Jobsheet 6/Sorting04.java
@@ -0,0 +1,35 @@
+public class Sorting04 {
+    int [] data;
+    int jumData;
+
+    Sorting04(int Data[], int jmlData) {
+        jumData = jmlData;
+        data = new int[jmlData];
+        for (int i = 0; i < jmlData; i++) {
+            data[i] = Data[i];
+
+        }
+    }
+
+
+    void bubbleSort() {
+        int temp = 0;
+        for (int i = 0; i < jumData - 1; i++) {
+            for (int j = 1; j < jumData - i; j++) {
+                if (data[j-1] > data[j]) {
+                    temp = data[j];
+                    data[j] = data[j-1];
+                    data[j-1] = temp;
+                }
+            }
+        }
+    }
+
+
+    void tampil() {
+        for (int i = 0; i < jumData; i++) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ Jobsheet 6/SortingMain04.java
@@ -0,0 +1,12 @@
+public class SortingMain04 {
+    public static void main(String[] args) {
+        int a[] = {20,10,2,7,12};
+        Sorting04 dataurut1 = new Sorting04(a, a.length);
+
+        System.out.println("Data awal 1");
+        dataurut1.tampil();
+        dataurut1.bubbleSort();
+        System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
+        dataurut1.tampil();
+    }
+}
