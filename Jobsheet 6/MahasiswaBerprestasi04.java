a27cedd68191ef1de1187d732b11dc8d1bc5ca1d
sopyanbadawi <sopyanbadawi.com>
2025-03-28 17:40:39 +0700

    Modifikasi Insertion (ASC) menjadi (DSC)

--- Jobsheet 6/MahasiswaBerprestasi04.java
+++ Jobsheet 6/MahasiswaBerprestasi04.java
@@ -49,7 +49,7 @@ public class MahasiswaBerprestasi04 {
         for (int i = 0; i < listMhs.length; i++) {
             Mahasiswa04 temp = listMhs[i];
             int j = i;
-            while (j > 0 && listMhs[j-1].ipk > temp.ipk) {
+            while (j > 0 && listMhs[j-1].ipk < temp.ipk) {
                 listMhs[j] = listMhs[j-1];
                 j--;
             }
