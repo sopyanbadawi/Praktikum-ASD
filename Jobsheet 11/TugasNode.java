43aff7dc01de566ea8a7a94755391c4072f8aa6d
sopyanbadawi <sopyanbadawi.com>
2025-05-31 19:41:34 +0700

    Tugas

---
+++ Jobsheet 11/TugasNode.java
@@ -0,0 +1,9 @@
+public class TugasNode {
+    TugasMahasiswa data;
+    TugasNode next;
+
+    TugasNode(TugasMahasiswa data, TugasNode next) {
+        this.data = data;
+        this.next = next;
+    }
+}
